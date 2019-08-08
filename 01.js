30184b886643c89ace983cf39a79c67052074023
Cameron Twiggs <CamTwiggs@gmail.com>
2019-08-08 15:23:31 -0400

    BRuh

--- 01.js
+++ 01.js
@@ -16,6 +16,18 @@ Bonus - ES6 Syntax: Use ES6 Syntax in your answer.
   Feel free to add on new methods or properties to incorporate the syntax.
 */
 
+
+
+
+
+
+
+
+
+
+
+
+
 function Person(name, job, age) {
     this.name = name;
     this.job = job;
